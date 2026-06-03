fef3fc4034473790c4d8fb473561892abb1d5f29
clearpyh <16249332+clearpyh@user.noreply.gitee.com>
2026-06-03 13:47:04 +0800

    根据员工id查询员工，编辑员工信息

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -29,4 +29,13 @@ public interface EmployeeMapper {
     //分页查询
 
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+//根据主键动态修改属性
+
+    void update(Employee employee);
+
+
+
+    //根据id查询员工
+    @Select("select * from employee where id = #{id}")
+    Employee getById(Long id);
 }
